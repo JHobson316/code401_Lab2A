23a9e784467ed8eecc9dc12a0d4a3650989162f4
Lizzyrho21 <lizzyrho@gmail.com>
2022-10-04 15:00:26 -0500

    Pushing in-class work

---
+++ Lab2A/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Program{
+    public class Example{
+        public static void Main(string[] args){
+            try{
+            Console.WriteLine("Welcome to the game! It's math time!");
+            StartSequence();
+            }
+            finally {
+                Console.WriteLine("Game Complete! Have great day!");
+            }
+        }
+        public static void StartSequence(){
+            string[] numArray = new string[6];
+            // probably need a for loop?
+            Console.WriteLine("Enter a number greater than zero");
+            int numhere = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numhere);
+            GetSum();
+        }
+        public static void GetSum(){
+            int sum = 0;
+            //for ()
+            Console.WriteLine($"Sum works {sum}");
+            GetProduct();
+        }
+        public static void GetProduct(){
+            int product = 0;
+            Console.WriteLine($"Product works {product}");
+            GetQuotient();
+        }
+        public static void GetQuotient(){
+            int quotient = 0;
+            Console.WriteLine($"Quotient works {quotient}");
+        }
+    }
+}
