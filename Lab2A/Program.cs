11bc4c8b8f34961743f4ef9127db2279429ee323
Jordan Hobson <hobsoncjordan@gmail.com>
2022-10-04 23:44:29 -0500

    Coded methods and made structure

--- Lab2A/Program.cs
+++ Lab2A/Program.cs
@@ -11,20 +11,37 @@ namespace Program{
             }
         }
         public static void StartSequence(){
-            string[] numArray = new string[6];
-            // probably need a for loop?
             Console.WriteLine("Enter a number greater than zero");
-            int numhere = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(numhere);
-            GetSum();
+            // Getting length of array
+            int length = Console.ReadLine();
+            try{
+            int[] inputArray = new int[length];
+            //Exception for if the input is invalid
+            } catch (Exception e){
+                Console.WriteLine(e.Message);
+            }
+            // Calling the next method
+            Populate(inputArray);
         }
-        public static void GetSum(){
+        public static int[] Populate(int[]){
+            int i;
+            for (i=0; i<int[].length; i++){
+                Console.WriteLine($"Enter number {i+1} of {inputArray.length}");
+                string input = Console.ReadLine();
+                inputArray[i] = input;
+            }
+            GetSum(inputArray)
+        }
+        public static void GetSum(array){
             int sum = 0;
-            //for ()
-            Console.WriteLine($"Sum works {sum}");
-            GetProduct();
+            int i;
+            //Creating a loop to add all 
+            for (i=0; i<array.length; i++){
+                sum = sum + Convert.ToInt32(array[i]);
+            }
+            GetProduct(sum);
         }
-        public static void GetProduct(){
+        public static void GetProduct(sum, ){
             int product = 0;
             Console.WriteLine($"Product works {product}");
             GetQuotient();
